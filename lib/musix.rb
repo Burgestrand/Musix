8d871aa90665f4228999a33e11c10b171912cfa7
Kim Burgestrand <kim@burgestrand.se>
2010-04-25 21:16:02 +0200

    Fix up the RDoc

--- lib/musix.rb
+++ lib/musix.rb
@@ -14,6 +14,7 @@ require 'cgi'
 # in the future.
 module Musix
   # = A Spotify metadata API wrapper
+  # 
   #
   # == Examples
   #   Service::Spotify::Search.track('Fix You')
@@ -23,25 +24,45 @@ module Musix
     format :xml
     base_uri 'http://ws.spotify.com/'
     
-    class Search #:nodoc:
-      [:track, :album, :artist].each do |method|
-        meta_def method do |query, *args|
-          Spotify::get("/search/1/#{method}", :query => { :q => query, :page => [args.first.to_i, 1].max }).fetch("#{method}s")
-        end
+    # = Spotify metadata search API
+    # Spotify::Search provides three methods for searching the {Spotify metadata API}[http://developer.spotify.com/en/metadata-api/search/].
+    class Search
+      
+      # Searches Spotify for a track.
+      def track(query, page = 1)
+        search("track", query, page)
+      end
+      
+      # Searches Spotify for an album.
+      def album(query, page = 1)
+        search("album", query, page)
+      end
+      
+      # Searches Spotify for an artist.
+      def artist(query, page = 1)
+        search("artist", query, page)
+      end
+      
+      private
+      def search(type, query, page)
+        Spotify::get("/search/1/#{method}", :query => { :q => query, :page => [args.first.to_i, 1].max }).fetch("#{method}s")
       end
     end
     
-    # Looks up a Spotify URI
+    # Looks up information about a Spotify URI with the +detail+ level of detail (max 2).
+    # 
+    # Spotify URIs looks like the following:
+    # -  spotify:track:id
+    # -  spotify:artist:id
+    # -  spotify:album:id
     def self.lookup(uri, detail = 0)
       type = uri.match('spotify:([^:]+)')[1]
       get '/lookup/1/', :query => { :uri => uri, :extras => [type, "detail"].slice(0, detail).join }
     end
   end
   
-  # Grooveshark API
-  #
-  # Example
-  #   - Service::Grooveshark.search 'FIX YOU'
+  # = Grooveshark API
+  # Searches Grooveshark using the {Tinysong API}[http://tinysong.com/api]
   class Grooveshark
     include HTTParty
     format :json
@@ -53,13 +74,15 @@ module Musix
     end
   end
   
-  # iTunes API
+  # = iTunes API
+  # This code would not have been possible if I hadn’t found {itms-lib}[http://itms-lib.rubyforge.org/].
+  # It made my quest for an iTunes search API much shorter.
   class ITunes
     include HTTParty
     headers 'User-Agent' => 'iTunes/9.1'
     format :plain
     
-    # Searches iTunes for the search-term
+    # Searches iTunes for +term+.
     def self.search(term)
       result = get 'http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZSearch.woa/wa/search?submit=edit&term=%s' % CGI::escape(term)
       itms   = Nokogiri::XML result
